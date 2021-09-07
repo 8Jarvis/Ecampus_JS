d5af90237ff247ce26ef2131a4f4b5a7eef98b51
8Jarvis <89958824+8Jarvis@users.noreply.github.com>
2021-09-07 22:10:44 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,46 @@
+function clearErrors(){
+    errors = document.getElementsByClassName('nameerror');
+    for(let item of errors){
+        item.innerHTML = "";
+    }
+}
+
+function seterror(id, error){
+    element = document.getElementById(id);
+    element.getElementsByClassName('nameerror')[0].innerHTML = error;
+
+}
+function validateForm(){
+    var returnval = true;
+    clearErrors();
+
+    //Perform validation and if validation fails, set the value of returnval to false
+    var name=document.forms['myForm']["studentName"].value;
+    if(name.length<5){
+        seterror("name", "*Length of name is too short");
+        returnval = false;
+    }
+
+    var email=document.forms['myForm']["studentEmail"].value;
+    if(email.length>20){
+        seterror("email", "*Email length is too long");
+        returnval = false;
+    }
+    return returnval;
+}
+
+function ageCalculator(){
+    var userinput = document.getElementById("DOB").value;
+    var dob = new Date(userinput);
+    if(userinput==null || userinput==''){
+        document.getElementsByClassName("nameerror").innerHTML = "*Choose a date!";
+        return false;
+    }
+    else{
+        var month_diff = Date.now() - dob.getTime();
+        var age_dt = new Date(month_diff);
+        var year = age_dt.getUTCFullYear();
+        var age = Math.abs(year - 1970);
+        return document.getElementById("Age").value=age;
+    }
+}
